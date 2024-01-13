717569a7bb61e0d6fc96d7206d30b0f5c307f068
Jhonn <jhonngutierrez.contact@gmail.com>
2024-01-13 14:33:22 -0500

    init repository

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const cleanVisuals = () => {
+  const contentContainer = document.getElementById("contentContainer")
+
+  if(contentContainer) {
+    contentContainer.removeAttribute("opened")
+  contentContainer.removeAttribute("swipe-open")
+  contentContainer.removeAttribute("persistent")
+  }
+
+const guide = document.getElementById("guide")
+  if (guide) {
+    guide.removeAttribute("opened")
+  guide.removeAttribute("swipe-open")
+  guide.removeAttribute("persistent")
+  }
+
+  const newTitle = document.title.split(") ")[1]
+  
+  if (newTitle) {
+    document.title = newTitle
+  }
+}
+
+cleanVisuals()
+console.log("Avocado")
+window.addEventListener("load", () => {
+    cleanVisuals()
+    setInterval(cleanVisuals, 1500)
+})
